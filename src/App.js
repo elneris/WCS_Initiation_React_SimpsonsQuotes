ee34a128cd23d0e61c3e3e4a752e24f3989434d8
Elneris Dang <elneris.dang@gmail.com>
2019-05-13 21:34:02 +0200

    Afficher une liste de composants

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import Quote from "./Composant/Quote";
+import Quote from "./Quote";
+import Quotes from "./Quotes";
 
 class App extends Component {
   render() {
@@ -12,16 +13,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
         </header>
-        <Quote
-          quote="I believe the children are the future... Unless we stop them now!"
-          character="Homer Simpson"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-        />
-        <Quote
-          quote="Me fail English? That's unpossible"
-          character="Ralph Wiggum"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
-        />
+        <Quote />
+        <Quotes />
       </div>
     );
   }
